ac0f1fca770be6965fdb3b3ba193214dd7b4a60f
deathman0001 <william7617063@gmail.com>
2025-03-12 11:38:27 +0800

    Modify main

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from A01 import A01
+from  student import Student
+from  A01 import A01
+from  A02 import Teacher
+a01 = A01('王老師')
+a02 = A01('朱老師')
+b01 = Teacher('系統架構與軟體工程服務','朱紹威','4106','星期三','02 - 04')
 
-b01 = A01('AA')
-b02 = A01('BB')
+student1 = Student('呂潍均')
+student2 = Student('蔡閎翔')
+student3 = Student('施宇傑')
+student1.getName()
+student2.getName()
+student3.getName()
+a01.getName()
+a02.getName()
+b01.get()
 
-
-b01.getName()
-b02.getName()
