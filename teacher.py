22570bfe2ee7df8ba6d9812c58fc0fdebfbc9a1a
pppppplay <thx.930110@gmail.com>
2025-03-19 11:37:35 +0800

    teacher
    choose teacher

---
+++ teacher.py
@@ -0,0 +1,13 @@
+teacher_name_list = ['王老師', '朱老師', '陳老師']
+
+for i in range(len(teacher_name_list)):
+    teacher = Teacher(teacher_name_list[i])
+    teacher_list.append(teacher)
+print(teacher_list)
+
+print("--------------------------------")
+for i in range(len(teacher_name_list)):
+    print(str(i), ':', teacher_name_list[i])
+print("--------------------------------")
+
+print('[Message] End')
