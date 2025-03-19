c748942633a29516ce54e4f586af4188d9837208
deathman0001 <william7617063@gmail.com>
2025-03-19 10:56:57 +0800

    main
    main change02

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from  student import Student
 from  A01 import Teacher
-b01 = Teacher('系統架構與軟體工程服務','朱紹威','4106','星期三','02 - 04')
 student1 = Student('呂潍均')
 student2 = Student('蔡閎翔')
 student3 = Student('施宇傑')
@@ -8,4 +7,5 @@ student1.getName()
 student2.getName()
 student3.getName()
 b01.get()
-
+#A01
+#b01 = Teacher('系統架構與軟體工程服務','朱紹威','4106','星期三','02 - 04')
