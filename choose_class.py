7eff7b9f86548d241a6f900146479002771663f3
deathman0001 <william7617063@gmail.com>
2025-03-26 11:58:00 +0800

    password and choose first

---
+++ choose_class.py
@@ -0,0 +1,15 @@
+import os
+import platform
+
+from student import Student
+
+
+
+class choose:
+    def __init__(self, A):
+        self.A = A
+    def chooseclass(self):
+        
+if __name__ == "__main__":
+    password = PassWord("A")
+    password.chooseclass()
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from  student import Student
-from  teacher import Teacher
+from student import Student
+from teacher import Teacher
+from password import PassWord
+
 student1 = Student('呂潍均')
 student2 = Student('蔡閎翔')
 student3 = Student('施宇傑')
 teacher1 = Teacher('A')
+password1 = PassWord('A')
+
+pass_acc = password1.getpassword()
+if(pass_acc == 1):
+    teacher1.getclass()
+
+
+
+"""
 student1.getName()
 student2.getName()
 student3.getName()
 teacher1.getclass()
-
-#A01
-#b01 = Teacher('系統架構與軟體工程服務','朱紹威','4106','星期三','02 - 04')
-#b01.get()
+A01
+b01 = Teacher('系統架構與軟體工程服務','朱紹威','4106','星期三','02 - 04')
+b01.get()
+"""
