6fb51870ebe1faf0b2bbdcc847f4d57b3a6d97da
deathman0001 <william7617063@gmail.com>
2025-03-26 10:34:11 +0800

    END02

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from  student import Student
-from  A01 import Teacher
+from  teacher import Teacher
 student1 = Student('呂潍均')
 student2 = Student('蔡閎翔')
 student3 = Student('施宇傑')
-
+teacher1 = Teacher('A')
 student1.getName()
 student2.getName()
 student3.getName()
-b01.get()
+teacher1.getclass()
+
 #A01
 #b01 = Teacher('系統架構與軟體工程服務','朱紹威','4106','星期三','02 - 04')
+#b01.get()
