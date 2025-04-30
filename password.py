6a51df4f80d1723ba8411fee50c2beee1c38d929
deathman0001 <william7617063@gmail.com>
2025-04-30 10:36:19 +0800

    password email choose add

--- password.py
+++ password.py
@@ -1,9 +1,10 @@
 import os
 import platform
+import re
 
 from student import Student
 
-password_save = {"1" : "1"}
+password_save = {"1@1" : "1"}
 
 class PassWord:
     def __init__(self, A):
@@ -15,14 +16,17 @@ class PassWord:
                 print("還剩下%d次機會"%A)
             print("輸入帳號:",end="")
             account = input()
+            if not re.match(r"^[^@]+@[^@]+$", account):
+                print(f"無效的email格式: {account}")
+                continue
             print("輸入密碼:",end="")
             pssword = input()
             if account in password_save:
                 if password_save[account] == pssword:
-                    os.system('cls')
+                    #os.system('cls')
                     print("登錄成功")
                     return 1
-            os.system('cls')
+            #os.system('cls')
             print("登錄失敗")
             A -= 1    
         print("鎖住")
@@ -30,3 +34,11 @@ class PassWord:
 if __name__ == "__main__":
     password = PassWord("A")
     password.getpassword()
+    
+"""
+def email(self,value):
+        if re.match(r"^[^@]+@[^@]+$",value)
+            self.email = valu
+        else:
+            return ValueError(f"無效的email格式:{value}")
+            """
